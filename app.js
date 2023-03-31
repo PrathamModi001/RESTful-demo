c0ab3bc4473c716b0b11d3bbd3f2fce96bc8fbe3
PrathamModi001 <prathammodi001@gmail.com>
2023-03-31 17:39:23 +0530

    cors error solved maybe ?

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 8080;
@@ -7,6 +7,13 @@ const PORT = 8080;
 app.use(bodyParser.json()); // application/json
 
 const feedRoutes = require('./routes/feed')
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 app.use('/feed' , feedRoutes) // now all feedRoutes routes will have /feed at the start
 
